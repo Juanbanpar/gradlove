f6fa64f68de897c15fc79d7b0596f52f0a78c52d
juanbanpar <jbanga@gradiant.org>
2024-03-06 19:03:13 +0100

    Open channel in browser

--- amor.ps1
+++ amor.ps1
@@ -106,6 +106,9 @@ Set-WallPaper -Image $WallpaperDest -Style Fit
 # Add to the clipboard LOVE gif
 Get-ChildItem $GifDest | Set-Clipboard
 
+# Open my favourite channel to express HOW MUCH LOVE I have
+start "https://teams.microsoft.com/l/channel/19%3acd6d7b457ff84d86b3a182a310139597%40thread.tacv2/Random?groupId=1a0ebcf0-5300-494f-a968-317e4e6c7a42&tenantId=66102552-ecf2-44f2-aeee-14fa85460e0f"
+
 # Sleep 5 minutes to wait for the LOVER to be back at the computer
 Start-Sleep -Seconds 300
 
